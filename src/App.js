497cb5e0360542fea8e97d764c943d57157d36b5
Muhammad Saqib <mehersaqib931@yahoo.com>
2021-02-22 20:55:15 +0500

    updated app.js for git actions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
     <h1> hello world by manual surge deployment</h1>
+    <h2> agin hello world bu using git actions</h2>
     </div>
   );
 }
